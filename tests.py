4a86625d1f58b0494476aa45b541c2407b5a9060
zacurrya <39706093+Zacurrya@users.noreply.github.com>
2026-04-18 14:43:27 +0100

    task 5 - wrote tests

---
+++ tests.py
@@ -0,0 +1,16 @@
+from app import update_line_chart
+import pandas as pd
+
+def test_header():
+    df = pd.read_csv('data/pink_morsel_sales.csv')
+    assert list(df.columns) == ['sales', 'date', 'region']
+
+def test_line_chart():
+    fig = update_line_chart('all')
+    assert fig.data is not None
+
+def test_region_radio():
+    options = ['all', 'north', 'south', 'east', 'west']
+    for option in options:
+        fig = update_line_chart(option)
+        assert fig is not None
